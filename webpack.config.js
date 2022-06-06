9cd7933a4e15bff3dc0ce24e0824620903bddbfb
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-06-06 17:46:18 -0400

    Better Title
    Changes
    Update the Webpack.config.js to not have Turbowarp in page titles

--- webpack.config.js
+++ webpack.config.js
@@ -158,28 +158,28 @@ module.exports = [
                 chunks: ['editor'],
                 template: 'src/playground/index.ejs',
                 filename: 'editor.html',
-                title: 'TurboWarp - Run Scratch projects faster',
+                title: 'SN-Edit - Develop like never before',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['player'],
                 template: 'src/playground/index.ejs',
                 filename: 'index.html',
-                title: 'TurboWarp - Run Scratch projects faster',
+                title: 'SN-Edit - Develop like never before',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['fullscreen'],
                 template: 'src/playground/index.ejs',
                 filename: 'fullscreen.html',
-                title: 'TurboWarp - Run Scratch projects faster',
+                title: 'SN-Edit - Develop like never before',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['embed'],
                 template: 'src/playground/index.ejs',
                 filename: 'embed.html',
-                title: 'Embedded Project - TurboWarp',
+                title: 'Embedded Project - Sn-Edit',
                 noTheme: true,
                 ...htmlWebpackPluginCommon
             }),
@@ -187,14 +187,14 @@ module.exports = [
                 chunks: ['addon-settings'],
                 template: 'src/playground/simple.ejs',
                 filename: 'addons.html',
-                title: 'Addon Settings - TurboWarp',
+                title: 'SN-Edit Addon Settings',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['credits'],
                 template: 'src/playground/simple.ejs',
                 filename: 'credits.html',
-                title: 'TurboWarp Credits',
+                title: 'SN-Edit Credits',
                 noSplash: true,
                 ...htmlWebpackPluginCommon
             }),
