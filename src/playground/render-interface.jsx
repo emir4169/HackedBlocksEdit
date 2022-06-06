6becff7786cddc6c5ac9f67977f89dffd9b695ec
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-06-06 19:43:30 -0400

    turbo no turbo

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -66,7 +66,7 @@ const handleClickAddonSettings = () => {
 
 const messages = defineMessages({
     defaultTitle: {
-        defaultMessage: 'SN-Edit - Develop like never before.',
+        defaultMessage: 'Develop like never before.',
         description: 'Title of homepage',
         id: 'tw.guiDefaultTitle'
     }
