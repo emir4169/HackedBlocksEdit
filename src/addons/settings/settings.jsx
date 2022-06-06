4b8dc45bce1b217c2156d74b9ce72abbc0a2a6ea
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-06-06 18:51:12 -0400

    letters
    Changes
    Attempt to fix tab naming issue

--- src/addons/settings/settings.jsx
+++ src/addons/settings/settings.jsx
@@ -55,7 +55,7 @@ if (locale !== 'en') {
     }
 }
 
-document.title = `${settingsTranslations.title} - TurboWarp`;
+document.title = `${settingsTranslations.title} - SN-Edit`;
 
 const theme = getInitialDarkMode() ? 'dark' : 'light';
 document.body.setAttribute('theme', theme);
